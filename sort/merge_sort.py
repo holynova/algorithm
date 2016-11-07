25a0749535bed47148f06c0e28d423679b661507
moonreader <holy_nova@126.com>
2016-11-07 17:09:36 +0800

    完成了堆排序的python实现

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -1,107 +1,146 @@
 import random,copy
 
-# arrClone = 
-# 
-class Merge:
-    # arrClone
-    # def __init__(self):
-        # self.arrClone = []
-    def mergeSort(self,arr):
-        self.arrClone =[None]*len(arr)
-        self.sort(arr,0,len(arr)-1)
-        return arr
-    def sort(self,arr,lo,hi):
-        if(lo>=hi):
-            return
-        mid = int((lo+hi)/2)
-        self.sort(arr,lo,mid)
-        self.sort(arr,mid+1,hi)
-        self.merge(arr,lo,mid,hi)  
-
-    def merge(self,arr,lo,mid,hi):
-        for cnt in range(lo,hi+1):
-            self.arrClone[cnt] = arr[cnt]
-        leftPointer = lo
-        rightPointer = mid+1
-        for cnt in range(lo,hi+1):
-            if leftPointer>mid:
-                arr[cnt] = arrClone[rightPointer]
-                rightPointer += 1
-            elif rightPointer >hi:
-                arr[cnt] = arrClone[leftPointer]
-                leftPointer +=1
-            elif arrClone[rightPointer]<arrClone[leftPointer]:
-                arr[cnt] = arrClone[rightPointer]
-                rightPointer += 1
-            else:
-                arr[cnt] = arrClone[leftPointer]
-                leftPointer +=1
-
-
-m =  Merge();
-
-def test():
-    arr = randArr(100)
-    arrCopy = copy.deepcopy(arr)
-    m.mergeSort(arr)
-    if arr == arrCopy:
-        print 'correct'
-    else:
-        print 'error'
-
-def merge(arr,lo,mid,hi):
-    arrClone = copy.deepcopy(arr)
-    # for cnt in range(lo,hi+1):
-    #     arrClone[cnt] = arr[cnt]
-    leftPointer =lo
-    rightPointer = mid+1
-    for cnt in range(lo,hi+1):
-        if leftPointer>mid:
-            arr[cnt] = arrClone[rightPointer]
-            rightPointer += 1
-        elif rightPointer >hi:
-            arr[cnt] = arrClone[leftPointer]
-            leftPointer +=1
-        elif arrClone[rightPointer]<arrClone[leftPointer]:
-            arr[cnt] = arrClone[rightPointer]
-            rightPointer += 1
+def fullTest(func,times=100,arrSize=1000,arrMin=-1000,arrMax=1000):
+    countRight = 0
+    countWrong = 0
+    for i in range(times):
+        if testSort(func,arrSize,arrMin,arrMax):
+            countRight += 1
         else:
-            arr[cnt] = arrClone[leftPointer]
-            leftPointer +=1
+            countWrong += 1
+    print '%s test: right: %d, wrong: %d' %(func.__name__,countRight,countWrong)
 
 
-
-        
-def sort(arr,lo,hi):
-    if(lo>=hi):
-        return
-    mid = int((lo+hi)/2)
-    sort(arr,lo,mid)
-    sort(arr,mid+1,hi)
-    merge(arr,lo,mid,hi)    
-
-def mergeSort(arr):
-    length = len(arr)
-    arrClone = [None]*length
-    sort(arr,0,length)
-    return arr
-
-def randArr(N = 1000,min = -100,max = 100):
+def randArr(N = 1000,min = -1000,max = 1000):
     return [random.randrange(min,max) for i in range(N)]
 
-def testSort(funcName=''):
-    arr = randArr(100)
+def testSort(func,N=100000,min=-1000,max=1000):
+    arr = randArr(N,min,max)
     arrCopy = copy.deepcopy(arr)
-    eval(funcName+'(arr)')
-    quickSort(arr)
-    # print arr
+    func(arr)
     arrCopy.sort()
-    # print arrCopy
+    resStr = '%s tested with a %d array ,result: ' %(func.__name__,N)
     if arr == arrCopy:
-        print 'correct'
+        # print resStr + 'CORRECT'
+        return True
     else:
-        print 'error'
+        # print resStr+ 'ERROR'
+        print arr
+        print arrCopy
+
+        return False
+
+def mergeSort(arr):
+    arr2 = [None]*len(arr)
+    def merge(arr,lo,mid,hi):
+        for i in range(lo,hi+1):
+            arr2[i] = arr[i]
+        pleft = lo
+        pright = mid+1
+        for i in range(lo,hi+1):
+            if pleft>mid:
+                arr[i] = arr2[pright]
+                pright +=1
+            elif pright > hi:
+                arr[i] = arr2[pleft]
+                pleft += 1
+            elif arr2[pright] < arr2[pleft]:
+                arr[i] = arr2[pright]
+                pright += 1
+            else:
+                arr[i] = arr2[pleft]
+                pleft += 1
+    def sort(arr,lo,hi):
+        if lo>=hi:
+            return
+        mid = int((lo+hi)/2)
+        sort(arr,lo,mid)
+        sort(arr,mid+1,hi)
+        merge(arr,lo,mid,hi)
+    sort(arr,0,len(arr)-1)
+
+def quickSort(arr):
+    def swap(arr,i,j):
+        arr[i],arr[j] = arr[j],arr[i]
+    def shuffle(arr):
+        for i in range(len(arr)-1,0,-1):
+            swap(arr,i,random.randrange(0,i))
+        return arr
+    def partition(arr,lo,hi):
+        storedIndex = lo
+        pivot = arr[hi]
+        for i in range(lo,hi):
+            if arr[i]<pivot:
+                swap(arr,i,storedIndex)
+                storedIndex += 1
+        swap(arr,storedIndex,hi)
+        return storedIndex
+    def partition2(arr,lo,hi):
+        if hi - lo <= 1:
+            return 
+        pivot = arr[lo]
+        pleft = lo
+        pright = hi
+        while True:
+            while arr[pleft] <= pivot and pleft <= pright:
+                pleft += 1
+              
+            while arr[pright] >= pivot and pleft <= pright:
+                pright -= 1
+             
+            if pleft > pright:
+                break
+            else:
+                swap(arr,pleft,pright)
+        swap(arr,lo,pright)
+        return pright
+
+    def sort(arr,lo,hi):
+        if lo >= hi:
+            return
+        storedIndex = partition(arr,lo,hi)
+        sort(arr,lo,storedIndex-1)
+        sort(arr,storedIndex+1,hi)
+    shuffle(arr)
+    sort(arr,0,len(arr)-1)
+    return arr
 
 
-# testSort('mergeSort')
-test()
+def heapSort(arr):
+    arr.insert(0,'-')
+    def swap(arr,i,j):
+        arr[i],arr[j] = arr[j],arr[i]
+    def makeHeap(arr):
+        index = int(len(arr)/2)
+        for i in range(index,0,-1):
+            sink(arr,i,len(arr)-1)
+
+    def sink(arr,index,max):
+        while index*2 <= max:
+            maxSon = index*2
+            if maxSon<max and arr[maxSon]<arr[maxSon+1]:
+                maxSon += 1
+            if arr[index] > arr[maxSon]:
+                break
+            else:
+                swap(arr,index,maxSon)
+                index = maxSon
+
+    def sort(arr):
+        makeHeap(arr)
+        for i in range(len(arr)-1,1,-1):
+            swap(arr,1,i)
+            sink(arr,1,i-1)
+
+    sort(arr)
+    arr.pop(0)
+    return arr
+
+# testCondition = (times=100,arrSize=100,arrMin=-1000,arrMax=1000)
+testCondition = (heapSort,100,100,-1000,1000)
+
+fullTest(heapSort,times = 1000)
+fullTest(quickSort,times = 1000)
+fullTest(mergeSort,times = 1000)
+# fullTest(quickSort,testCondition)
+# fullTest(mergeSort,testCondition)
